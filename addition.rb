279ffadaf36f1009f36f59ec2103c89da91a19e3
Ysabel Bombardiere <ysabelestradab@gmail.com>
2016-09-26 12:18:19 -0400

    app update 9.26

---
+++ addition.rb
@@ -0,0 +1,8 @@
+def addition (n1,n2,n3)
+
+	total_sum = n1+n2+n3
+	puts total_sum
+	
+end
+
+addition(1,2,3)
--- app.rb
+++ app.rb
@@ -1,33 +1,61 @@
 require 'sinatra'
+require_relative "addition.rb"
 
-# get '/' do #roots of the url
-# 	"Hello CRW!" 
-# 	"how are you"
-# 	"*******************************   ***** ***** **** *** <<<<<<<<<<<>>>>>>>>>>>>>>>>>>>> ************"
-# end
-
-get '/' do
-	erb :get_name
+get '/' do #create url
+	erb :get_name #open file get_name.erb
 
 end
 
-# post '/uname' do
-# 	tname = params[:auser_name]
-#   "Hello #{tname} World"
-# end
 
 
-post '/uname' do
-	uname = params[:auser_name]
-  	redirect '/age?ausers_name=' + uname
+post '/name' do  #create url for name
+	name = params[:auser_name] #put :auser_name to browser, name is connected to /name. key :auser_name is saved by name.
+  	redirect '/age?ausers_name=' + name #add name to browser, age?userc_name=name
+  
 end
 
-# get '/age' do
-# 	uname = params[:ausers_name]
-# 	"well hello #{uname}"
-# end
-
-get '/age' do
-	uname = params[:ausers_name]
-	erb :get_age, :locals => {:uname => uname}
+get '/age' do #run get_age.erb file
+	name = params[:ausers_name] #
+	erb :get_age, :locals => {:name => name} # :locals is for <%= name %=> is used on the erb file by itself
+	
 end
+
+# post '/age' do #create url /age
+# 	name = params[:auser_name] 
+# 	age = params[:auser_age]
+# 	"greeeat! name #{name} and you are #{age}" #get name from params
+# 	#redirect '/fav?ausers_name=' + name 
+
+
+# end
+# post '/age' do #create url /age
+# 	name = params[:auser_name] 
+# 	age = params[:auser_age]
+# 	"greeeat! name #{name} and you are #{age}" #get name from params
+# 	redirect '/fav?ausers_name=' + name 
+
+
+# end
+post '/age' do #create url /age
+	name = params[:auser_name] 
+	# age = params[:auser_age]
+	# "greeeat! name #{name} and you are #{age}" #get name from params
+	redirect '/fav?ausers_name=' + name 
+end
+
+get '/fav' do
+	name = params[:auser_name]
+	fav = params[:user_fav]
+	erb :get_fav, :locals => {:fav => fav}
+
+end
+
+post '/fav' do
+	name = params[:auser_name]
+	fav = params[:fav_one]
+	"greeeat! #{name} and you are #{fav}"
+
+end
+
+
+
