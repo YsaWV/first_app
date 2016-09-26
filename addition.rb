a6760ceee94df9c03fed35bd930aa5ecbc6dd286
Ysabel Bombardiere <ysabelestradab@gmail.com>
2016-09-26 15:15:12 -0400

    update

--- addition.rb
+++ addition.rb
@@ -1,8 +1,16 @@
 def addition (n1,n2,n3)
 
-	total_sum = n1+n2+n3
-	puts total_sum
+	total_sum = n1 + n2 + n3
+	
 	
 end
 
-addition(1,2,3)
+#puts addition(1,1,3)
+
+# def add(*numbers)
+# 	suma = 0
+# 	numbers.each do |number|
+# 		suma = suma + number
+# 	end
+# 	suma
+# end
--- app.rb
+++ app.rb
@@ -10,51 +10,45 @@ end
 
 post '/name' do  #create url for name
 	name = params[:auser_name] #put :auser_name to browser, name is connected to /name. key :auser_name is saved by name.
-  	redirect '/age?ausers_name=' + name #add name to browser, age?userc_name=name
+  	redirect '/age?auser_name=' + name #add name to browser, age?userc_name=name
   
 end
 
 get '/age' do #run get_age.erb file
-	name = params[:ausers_name] #
+	name = params[:auser_name] #
 	erb :get_age, :locals => {:name => name} # :locals is for <%= name %=> is used on the erb file by itself
 	
 end
 
-# post '/age' do #create url /age
-# 	name = params[:auser_name] 
-# 	age = params[:auser_age]
-# 	"greeeat! name #{name} and you are #{age}" #get name from params
-# 	#redirect '/fav?ausers_name=' + name 
 
-
-# end
-# post '/age' do #create url /age
-# 	name = params[:auser_name] 
-# 	age = params[:auser_age]
-# 	"greeeat! name #{name} and you are #{age}" #get name from params
-# 	redirect '/fav?ausers_name=' + name 
-
-
-# end
 post '/age' do #create url /age
+	age = params[:age]
 	name = params[:auser_name] 
-	# age = params[:auser_age]
-	# "greeeat! name #{name} and you are #{age}" #get name from params
-	redirect '/fav?ausers_name=' + name 
+	#"greeeat! name #{name} and you are #{age}" #get name from params
+	redirect '/fav?auser_name=' + name + "&age=" + age
 end
 
 get '/fav' do
+	age = params[:age]
 	name = params[:auser_name]
-	fav = params[:user_fav]
-	erb :get_fav, :locals => {:fav => fav}
+	
+	#fav = params[:user_fav]
+	erb :get_fav, :locals => {:name => name, :age => age}
 
 end
 
 post '/fav' do
 	name = params[:auser_name]
-	fav = params[:fav_one]
-	"greeeat! #{name} and you are #{fav}"
+	age = params[:age].to_i
+	fav1 = params[:fav_one].to_i
+	fav2 = params[:fav_two].to_i
+	fav3 = params[:fav_three].to_i
+	sum = addition(fav1,fav2,fav3) 
 
+	"greeeat! #{age} and you are #{fav1},#{fav2},#{fav3} is equal to #{sum} "
+
+	#puts "Greater than" if age > sum
+#<input type = "text" name = "total" value = <%= total %>>
 end
 
 
